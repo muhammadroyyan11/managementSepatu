e7edc1817334863b174d71e3cfe35e9c9e04330e
unknown <royyanmz87@gmail.com>
2021-11-05 16:58:24 +0700

    tampil data barang

--- application/controllers/Sepatu.php
+++ application/controllers/Sepatu.php
@@ -3,11 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Sepatu extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+        // cek_login();
+        date_default_timezone_set('Asia/Jakarta');
+        // // $this->load->model('Auth_model', 'auth');
+        $this->load->model('Base_model', 'base');
+    }
 
 	public function index()
 	{
 		// $this->load->view('template');
 		$data['title'] = 'Data Sepatu';
+        $data['barang'] = $this->base_model->get('barang')->result();
 		$this->template->load('template', 'sepatu/data', $data);
 	}
 }
--- application/views/sepatu/data.php
+++ application/views/sepatu/data.php
@@ -8,7 +8,7 @@
                 </h4>
             </div>
             <div class="col-auto">
-                <a href="<?= base_url('barang/add') ?>" class="btn btn-sm btn-primary btn-icon-split">
+                <a href="#" class="btn btn-sm btn-primary btn-icon-split" data-toggle="modal" data-target="#addModal">
                     <span class="icon">
                         <i class="fa fa-plus"></i>
                     </span>
@@ -24,33 +24,81 @@
             <thead>
                 <tr>
                     <th>No. </th>
-                    <th>ID Barang</th>
                     <th>Nama Barang</th>
-                    <th>Jenis Barang</th>
                     <th>Stok</th>
-                    <th>Satuan</th>
+                    <th>Harga</th>
                     <th>Aksi</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
                 $no = 1;
-                
-                        ?>
-                        <tr>
-                            <td><?= $no++; ?></td>
-                            <td>tes</td>
-                            <td>tes</td>
-                            <td>tes</td>
-                            <td>tes</td>
-                            <td>tes</td>
-                            <td>
-                                <a href="<?= base_url('barang/edit/')?>" class="btn btn-warning btn-circle btn-sm"><i class="fa fa-edit"></i></a>
-                                <a onclick="return confirm('Yakin ingin hapus?')" href="<?= base_url('barang/delete/')?>" class="btn btn-danger btn-circle btn-sm"><i class="fa fa-trash"></i></a>
-                            </td>
-                        </tr>
-                    
+                foreach ($barang as $key => $data) {
+                ?>
+                    <tr>
+                        <td><?= $no++; ?></td>
+                        <td><?= $data->nama ?></td>
+                        <td><?= $data->stok ?></td>
+                        <td><?= ' Rp. ' . number_format($data->harga)  ?></td>
+                        <td>
+                            <a href="<?= base_url('barang/edit/') ?>" class="btn btn-success btn-circle btn-sm"><i class="fa fa-eye"></i></a>
+                            <a href="<?= base_url('barang/edit/') ?>" class="btn btn-warning btn-circle btn-sm"><i class="fa fa-edit"></i></a>
+                            <a onclick="return confirm('Yakin ingin hapus?')" href="<?= base_url('barang/delete/') ?>" class="btn btn-danger btn-circle btn-sm"><i class="fa fa-trash"></i></a>
+                        </td>
+                    </tr>
+                <?php } ?>
             </tbody>
         </table>
+    </div>
+</div>
+
+<!-- Add Modal-->
+<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Data Sepatu</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">×</span>
+                </button>
+            </div>
+            <div class="modal-body">
+            <div class="card-body">
+                <?= $this->session->flashdata('pesan'); ?>
+                <?= form_open('', [], ['stok' => 0]); ?>
+                <div class="row form-group">
+                    <label class="col-md-3 text-md-right" for="nama_barang">Nama Barang</label>
+                    <div class="col-md-9">
+                        <input value="<?= set_value('nama_barang'); ?>" name="nama_barang" id="nama_barang" type="text" class="form-control" placeholder="Nama Barang...">
+                        <?= form_error('nama_barang', '<small class="text-danger">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <label class="col-md-3 text-md-right" for="satuan_id">Satuan Barang</label>
+                    <div class="col-md-9">
+                        <div class="input-group">
+                            <select name="satuan_id" id="satuan_id" class="custom-select">
+                                <option value="" selected disabled>Pilih Satuan Barang</option>
+                                <?php foreach ($satuan as $s) : ?>
+                                    <option <?= set_select('satuan_id', $s['id_satuan']) ?> value="<?= $s['id_satuan'] ?>"><?= $s['nama_satuan'] ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <div class="input-group-append">
+                                <a class="btn btn-primary" href="<?= base_url('satuan/add'); ?>"><i class="fa fa-plus"></i></a>
+                            </div>
+                        </div>
+                        <?= form_error('satuan_id', '<small class="text-danger">', '</small>'); ?>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-md-9 offset-md-3">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="reset" class="btn btn-secondary">Reset</bu>
+                    </div>
+                </div>
+                <?= form_close(); ?>
+            </div>
+            </div>
+        </div>
     </div>
 </div>
