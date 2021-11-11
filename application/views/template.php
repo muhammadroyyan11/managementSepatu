fd7cfec932003e34f9ead270fdd30e8660a077a4
unknown <royyanmz87@gmail.com>
2021-11-11 15:28:44 +0700

    fix

--- application/views/template.php
+++ application/views/template.php
@@ -151,10 +151,6 @@
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
-                                <a class="dropdown-item" href="<?= base_url('profile/changePhoto'); ?>">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Change Photo
-                                </a>
                                 <a class="dropdown-item" href="<?= base_url('profile/setting'); ?>">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Settings
